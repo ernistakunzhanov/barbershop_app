04aebb1be254bdc23f60f9a1f7c4565c87ca9baa
Ernist Akunzhanov <akunzhanovernist@gmail.com>
2023-07-20 18:36:29 +0600

    removed theme

--- lib/app.dart
+++ lib/app.dart
@@ -6,13 +6,9 @@ class BarbershopApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const HomeView(),
+      home: HomeView(),
     );
   }
 }
